dcd9b5f0ce0cafbea9dfe1903c1453a0f0fcd09a
karencplara <34969669+karencplara@users.noreply.github.com>
2018-06-04 13:32:29 -0300

    Initial commit

---
+++ Aplplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDbContext context = new ProductDbContext();
+        }
+    }
+}
---
+++ Data/ProductDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Domain;
+
+namespace Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(): base("Name=ProductDb")
+        {
+            Database.SetInitializer<ProductDbContext>
+                (new CreateDatabaseIfNotExists<ProductDbContext>());
+            Database.Initialize(false);
+        }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Loja> Lojas { get; set; }
+    }
+}
